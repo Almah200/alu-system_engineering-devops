ae4b3f36a5f48bee1cdb9f37273d3a8db5b6c71e
Almarat <arnualmarat@gmail.com>
2022-12-01 13:34:55 +0300

    0

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #script to solve the 500 error
+
 exec {'replce':
-  provider=> shell,
-  command=> 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
+  provider => shell,
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+ }
